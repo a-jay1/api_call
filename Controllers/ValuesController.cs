ec7ab88f8d49d94eb5acdced7106e2a2fdbc75d0
a-jay1 <ajay.alagesan@vakilsearch.com>
2023-07-13 13:13:22 +0530

    intit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication11.Models;
+
+namespace WebApplication11.Controllers
+{
+    public class ValuesController : ApiController
+    {
+
+        // GET api/values/5
+        public int Get()
+        {
+            return Model.getExpense("ajay");
+        }
+        // GET api/values
+        public int Get(int i)
+        {
+            return Model.getExpense("ajay");
+        }
+
+        
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Models/model.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace WebApplication11.Models
+{
+    public class Model
+    {
+        private static string cs = "Data Source=DESKTOP-3EIFCQR;Initial Catalog=expenseDB;Integrated Security=True";
+            //= WebConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+
+        public SqlConnection getConnection()
+        {
+            return new SqlConnection(cs);
+        }
+
+        public static int getExpense(string name)
+        {
+            Model db = new Model();
+            using (var connection = db.getConnection())
+            {
+                try
+                {
+                    connection.Open();
+
+                    string query = "SELECT SUM(cost_) FROM expense WHERE name_ = @name ";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@name", name);
+                    return (int)command.ExecuteScalar();
+
+                }
+                catch
+                {
+                    return 0;
+                }
+            }
+        }
+
+    }
+}
